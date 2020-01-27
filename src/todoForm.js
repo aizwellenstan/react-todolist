a4fe3a6d8b3584a3cdf14b87e20507c05137412f
aizwellenstan <aizwellenstan@gmail.com>
2020-01-28 01:01:43 +0900

    fix datepicker css

--- src/todoForm.js
+++ src/todoForm.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import DatePicker from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
-
 export class TodoForm extends React.Component {
     constructor() {
         super();
